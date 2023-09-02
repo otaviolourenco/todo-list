e7d2c5d831352d47875c06d6e73be89c726488e9
Otávio Lourenço <otavioluissv@gmail.com>
2023-09-02 20:21:35 +0100

    todo-list
    First project in React. Developed with the MDN Web Docs.

---
+++ src/components/FilterButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function FilterButton(props) {
+  return (
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
+    >
+      <span className="visually-hidden">Ver </span>
+      <span>{props.name}</span>
+      <span className="visually-hidden"> tarefas</span>
+    </button>
+  );
+}
+
+export default FilterButton;
---
+++ src/components/Form.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function Form(props) {
+  const [name, setName] = useState('');
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name.trim()) {
+      // impedirá submeter o form se o input estiver vazio
+      return;
+    }
+    props.addTask(name);
+    setName('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          Nova tarefa
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Adicionar
+      </button>
+    </form>
+  );
+}
+
+export default Form;
